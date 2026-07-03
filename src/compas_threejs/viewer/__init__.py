92ce44f29c733791f4e6df0826462299295e0779
Eric <ericgozzi@cosmicgarage.ch>
2026-07-03 09:18:13 +0200

    Update __init__.py
    Signed-off-by: Eric <ericgozzi@cosmicgarage.ch>

--- src/compas_threejs/viewer/__init__.py
+++ src/compas_threejs/viewer/__init__.py
@@ -1,11 +1,6 @@
-from .remote_viewer import RemoteViewer
 from .viewer import CameraView, Viewer
 
 __all__ = [
-    "Server",
     "CameraView",
     "Viewer",
-    "RemoteViewer",
-    "MessageReceiver",
-    "MessageSender",
 ]
