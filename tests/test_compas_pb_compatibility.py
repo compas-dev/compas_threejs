e3baf33f2a17835d305c301761181f6f927158d5
Gonzalo Casas <casas@arch.ethz.ch>
2026-08-10 18:54:26 +0200

    chore: require compas protobuf 1.x

---
+++ tests/test_compas_pb_compatibility.py
@@ -0,0 +1,48 @@
+import unittest
+
+import compas_pb
+from compas.geometry import Box
+from compas.geometry import Frame
+from compas.geometry import Point
+from compas.geometry import Vector
+
+from compas_threejs.viewer.outbox import Outbox
+
+
+class OfflineServer:
+    def get_loop(self):
+        return None
+
+
+class CompasPbCompatibilityTest(unittest.TestCase):
+    def test_geometry_roundtrip(self):
+        box = Box(
+            1,
+            2,
+            3,
+            Frame(Point(4, 5, 6), Vector(1, 0, 0), Vector(0, 1, 0)),
+        )
+
+        decoded = compas_pb.pb_load_bts(compas_pb.pb_dump_bts(box))
+
+        self.assertIsInstance(decoded, Box)
+        self.assertEqual((decoded.xsize, decoded.ysize, decoded.zsize), (1.0, 2.0, 3.0))
+        self.assertEqual(tuple(decoded.frame.point), (4.0, 5.0, 6.0))
+
+    def test_outbox_command_roundtrip(self):
+        outbox = Outbox(OfflineServer())
+        command = {"dispatch": "spinner", "visible": True, "message": "Loading"}
+
+        outbox.send_dict(command)
+
+        self.assertEqual(len(outbox._queue), 1)
+        binary_data, object_id, persist, workspace_id, remove_key = outbox._queue[0]
+        self.assertEqual(compas_pb.pb_load_bts(binary_data), command)
+        self.assertEqual(object_id, "")
+        self.assertFalse(persist)
+        self.assertEqual(workspace_id, "main")
+        self.assertIsNone(remove_key)
+
+
+if __name__ == "__main__":
+    unittest.main()
